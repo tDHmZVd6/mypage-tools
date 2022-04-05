4773773aca1a7a0e7d4f9a1e0d9916741684bfb3
tDHmZVd6 <102803393+tDHmZVd6@users.noreply.github.com>
2022-04-05 21:25:56 +0900

    Update Kyuuka.py

--- Kyuuka.py
+++ Kyuuka.py
@@ -7,6 +7,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 # List of day off
 disable_days = [
+# 勤務不可に設定する日付を'yyyy/mm/dd 0:00:00'形式で設定する
 #    '2021/12/30 0:00:00',
 #    '2022/01/01 0:00:00',
 #    '2022/01/02 0:00:00',
@@ -57,60 +58,6 @@ sid = res.text[sid_index + 4 : sid_index + 30]
 
 login_index = res.text.find('LOGIN_INFO=')
 login_info = res.text[login_index + 11 : login_index + 61]
-
-## Comment out ## 
-'''
-## Reserve Data
-reserve_param = {
-    'SID'        : sid,
-    'TOP_DATE'   : TOP_DATE,
-    'DATE_FLG'   : 'True',
-    'BTN'        : '1',
-    'LOGIN_INFO' : login_info
-}
-
-## Change Data
-change_param = {
-    'CHK_RSV'    : all_days,
-    'BTN'        : '1',
-    'SID'        : sid,
-    'TOP_DATE'   : TOP_DATE,
-    'DATE_FLG'   : 'True',
-    'LOGIN_INFO' : login_info
-}
-
-## Ret Change Data
-ret_param = {
-    'ERDO'                 : 'ENABLE_DAY', 
-    'DISABLE_HOUR_START'   : '00',
-    'DISABLE_MINUTE_START' : '00',
-    'DISABLE_HOUR_END'     : '00',
-    'DISABLE_MINUTE_END'   : '00',
-    'ENABLE_HOUR_START'    : '00',
-    'ENABLE_MINUTE_START'  : '00',
-    'ENABLE_HOUR_END'      : '00',
-    'ENABLE_MINUTE_END'    : '00',
-    'SID'                  : sid,
-    'TOP_DATE'             : TOP_DATE,
-    'LOGIN_INFO'           : login_info
-}
-
-## Reserve Action
-res = session.post(reserve_url, data=change_param)
-print('reserve_status:'+str(res.status_code))
-print(res.text)
-
-## Change Select Action
-res = session.post(change_url, data=change_param)
-print('select_status:'+str(res.status_code))
-print(res.text)
-
-# Change Commit Action
-res = session.post(ret_change_url, data=ret_param)
-print('change_status:'+str(res.status_code))
-print(res.text)
-'''
-## Comment End ##
 
 # Change Data
 change_param = {
